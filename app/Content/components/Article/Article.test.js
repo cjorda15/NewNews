2439275cb070cf7e223dc0bbdb3d3a63aba6d699
Chris Jordan <cjorda15@msudenver.edu>
2017-05-29 21:39:16 -0600

    added test folders to components

--- app/Content/components/Article/Article.test.js
+++ app/Content/components/Article/Article.test.js
@@ -1,10 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
 import Article from './Article'
-import { shallow, mount} from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 describe('Article test', () => {
 
   it('should render without crashing', () => {
-    const wrapper = shallow(Article)
+    const wrapper = shallow(<Article/>)
 
   })
 })
--- app/Content/components/List/ListContainer.js
+++ app/Content/components/List/ListContainer.js
@@ -2,13 +2,6 @@ import { connect } from 'react-redux'
 import List from './List'
 import {addSource} from '../../actions'
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     handleAddSource: source => {
-//       dispatch(addSource(source))
-//     }
-//   }
-// }
 
 const mapStateToProps = (state) => {
   return{
